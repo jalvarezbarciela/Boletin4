6f7f29bc06b164d43358c59875f088d02660d161
Jose Alvarez Barciela <jalvarezbarciela@nautilus6.danielcastelao.org>
2019-10-16 10:35:49 +0200

    bol4_1corregido

--- bol4_1/src/bol4_1/Bol4_1.java
+++ bol4_1/src/bol4_1/Bol4_1.java
@@ -1,11 +1,20 @@
 package bol4_1;
 
+import java.util.Scanner;
+
 public class Bol4_1 {
 
     public static void main(String[] args) {
+      
+      Scanner sc=new Scanner(System.in); 
       Coche coche = new Coche();
-      coche.acelerar(50);
-      coche.frenar(10);
+      int v;
+      System.out.println("Cuanto ha acelerado");
+      v=sc.nextInt();
+      coche.acelerar(v);
+      System.out.println("Cuanto ha frenado");
+      v=sc.nextInt();
+      coche.frenar(v);
       System.out.println(coche.getVelocidade());
     }
     
